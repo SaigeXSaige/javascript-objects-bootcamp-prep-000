83f9b9edc43f85d296fe6dea31cb5fe81d675d5e
Saige Leslie <saigeleslie@gmail.com>
2018-03-08 03:06:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName: songTitle}
+var playlist = {[artistName]: songTitle}
 function updatePlaylist(playlist, artistName, songTitle){
   newPlaylist = Object.assign({}, playlist, {[artistName]: songTitle});
   return newPlaylist;
