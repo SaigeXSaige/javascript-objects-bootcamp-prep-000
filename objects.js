cd5c7ad09aacf0aa730b958e7f899061f3915868
Saige Leslie <saigeleslie@gmail.com>
2018-03-08 02:08:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({}, {artistName: songTitle});
+  Object.assign({}, playlist, {artistName: songTitle});
 }
